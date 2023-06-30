1ec8b8496502b65e02a70df18163c962acdffe11
ydteng <291746925@qq.com>
2023-06-30 15:01:49 +0800

    第一次提交

---
+++ src/main/java/com/srtcrm/SrtCrmApplication.java
@@ -0,0 +1,13 @@
+package com.srtcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrtCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrtCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/srtcrm/SrtCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srtcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrtCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
